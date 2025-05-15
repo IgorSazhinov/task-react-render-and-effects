3aacf76b198ca591f3513a7cce08df32ebe29e94
ia.sazhinov@tensor.ru <ia.sazhinov@tensor.ru>
2025-05-15 07:26:31 +0300

    Проверка автотеста

--- src/Effects.tsx
+++ src/Effects.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import { subscribe, unsubscribe } from './resources/API';
 
 export function Effects(props: { sourceId: string }) {
+    const [lastMessage, setLastMessage] = useState<number>(-1);
+
+    useEffect(() => {
+        setLastMessage(-1);
+        // Обработчик для новых сообщений
+        const handleNewMessage = (message: number) => {
+            setLastMessage(message);
+        };
+
+        // Подписываемся на изменения текущего источника
+        subscribe(props.sourceId, handleNewMessage);
+
+        // При размонтировании или смене источника отписываемся
+        return () => {
+            unsubscribe(props.sourceId, handleNewMessage);
+        };
+    }, [props.sourceId]); // Эффект срабатывает при изменении sourceId
+
     return (
-        <>
-            <p>123</p>
-        </>
+        <div>
+            {props.sourceId}: {lastMessage}
+        </div>
     );
 }
